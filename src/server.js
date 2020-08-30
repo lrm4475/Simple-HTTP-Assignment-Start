96916673821bb49259669b86ffb9a90637444a27
lrm4475 <lrm4475@rit.edu>
2020-08-30 18:40:27 -0400

    Completed homework

--- src/server.js
+++ src/server.js
@@ -1,15 +1,40 @@
 const http = require('http');
-const fs  = require('fs');
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+const imgHandler = require('./imgResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
-    console.log(request.url);
-    
-    response.writeHead(200, { 'Content-type': 'text/html' });
-    response.write(index);
-    response.end();
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    case '/dankmemes':
+      imgHandler.getImg(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port);
